70cdf9b8c1735e53f9a2775b3f7c68904743bff5
SosukeAizen833 <Spandan833@gmai.com>
2022-02-09 21:36:48 +0530

    added colored congratuation message

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import inquirer from 'inquirer';
 import gradient from 'gradient-string';
 import chalkAnimation from 'chalk-animation';
 import {createSpinner} from 'nanospinner';
+import CFonts from 'cfonts';
 
 let playerName = 'player';
 const sleep = (ms = 2000) => {
@@ -15,7 +16,7 @@ async function welcome(){
         'Who wants to be a millionaire \n'
     );
 
-    await sleep(4000);
+    await sleep(2000);
     rainbowTitle.stop();
 
     console.log()
@@ -81,11 +82,26 @@ async function q3(){
     return handleAnswer(answers.q_3 == "Martin Guptill")
 }
 
+async function q4(){
+    const answers = await inquirer.prompt({
+        name: 'q_4',
+        type: 'list',
+        message: "Who has the most centuries in Womens ODI",
+        choices: [
+            'Mithali Raj',
+            'Heather Knight',
+            'Meg Lanning',
+            'CM Edwards',
+        ],
+
+    });
+    return handleAnswer(answers.q_4 == "Meg Lanning")
+}
+
 async function handleAnswer(isCorrect) {
     const spinner = createSpinner('Checking........');
     spinner.start();
     await sleep();
-
     if(isCorrect){
         spinner.success({text: `Congrats! ${playerName} Thats the right answrer3`})
     }else{
@@ -94,9 +110,27 @@ async function handleAnswer(isCorrect) {
     }
 }
 
+
 await welcome();
 await askName();
 await q1();
 await q2();
 await q3();
-console.log(gradient('cyan', 'pink')('YOU WIN!!!'));
+await q4();
+CFonts.say('Congrats!|You Win', {
+	font: 'block',              
+	align: 'center',              
+	colors: ['yellow','green'],         // define all colors
+	background: 'transparent',  // define the background color, you can also use `backgroundColor` here as key
+	letterSpacing: 1,           // define letter spacing
+	lineHeight: 1,              // define the line height
+	space: true,                // define if the output text should have empty lines on top and on the bottom
+	maxLength: '0',             // define how many character can be on one line
+	gradient: false,            // define your two gradient colors
+	independentGradient: false, // define if you want to recalculate the gradient for each new line
+	transitionGradient: false,  // define if this is a transition between colors directly
+	env: 'node'                 // define the environment CFonts is being executed in
+});
+
+
+
